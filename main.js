af00cc5e0ba892a78eef62ef3245d4bd29c52fcd
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-05-27 17:49:46 -0300

    🚀 Complete project (basic)

---
+++ main.js
@@ -0,0 +1,57 @@
+
+let map;
+
+window.initMap = async () => {
+
+  map = new google.maps.Map(document.querySelector('#map'), {
+    center: {lat: 37.41, lng: -122.12},
+    zoom: 11,
+    mapId: 'd28f54143940ba80',
+    styles: [
+      {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'all',
+        stylers: [{ visibility: 'off' }]
+      }
+    ]
+  })
+
+
+  locations.forEach((location) => {
+    const marker = new google.maps.Marker({
+      position: { lat: location.lat, lng: location.long},
+      map,
+      icon: {
+        url: location.iconBig,
+        scaledSize: new google.maps.Size(24, 24),
+      },
+      title: location.title,
+      gmpClickable: true
+    })
+
+
+    const infowindow = new google.maps.InfoWindow({
+			content: location.title,
+		});
+
+		marker.addListener('click', () => {
+			infowindow.open(map, marker);
+		});
+    
+  })
+  
+}
+
+const locations = [
+  {lat: 37.4220, long: -122.0841, iconBig: './icons8-google-100.svg', title: 'Googleplex'},
+  {lat: 37.4848, long: -122.1484, iconBig: './icons8-facebook-100.svg', title: 'Meta headquarters'},
+  {lat: 37.3349, long: -122.0090, iconBig: './icons8-apple-logo-100.svg', title: 'apple headquarters'},
+  {lat: 37.3947, long: -122.1503, iconBig: './icons8-tesla-100.svg', title: 'tesla headquarters'}
+];
+
+
